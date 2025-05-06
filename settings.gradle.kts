ba3e6d7d4b648f1b2bb51c8489b9ef2287b1928e
Luis Fernando <luisflima2020@gmail.com>
2025-05-06 09:36:16 -0300

    Initial Commit: setup architecture

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+
+include("rest-server")
