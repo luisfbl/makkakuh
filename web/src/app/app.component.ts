16ddfe1ce04b995b5d26e0a2468f59927180874a
pedrolucasrocha <pedrolucasferreirarocha8@gmail.com>
2025-05-06 22:27:24 -0300

    (feat) config inicial de roteamento das telas

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
---
+++ web/src/app/pages/QuemSomos/QuemSomos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quem-somos',
+  standalone: true,
+  templateUrl: './QuemSomos.component.html',
+  styleUrls: ['./QuemSomos.component.css']
+})
+export class QuemSomosComponent {}
